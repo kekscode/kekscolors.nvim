949588a4f2e571e35e1f0cc3b5254a065ddfadea
Stefan Antoni <stefan.antoni@deutschebahn.com>
2024-03-18 19:17:14 +0100

    chore: add+commit+pull+push all changes

--- lua/lush_theme/kekscolors.lua
+++ lua/lush_theme/kekscolors.lua
@@ -161,17 +161,17 @@ local theme = lush(function(injected_functions)
 
     Comment { fg = nord_polar_night_4 }, -- Any comment
 
-    Constant { fg = nord_snow_storm_1 }, -- (*) Any constant
+    Constant { Normal },                 -- (*) Any constant
     -- String         { }, --   A string constant: "this is a string"
     -- Character      { }, --   A character constant: 'c', '\n'
     -- Number         { }, --   A number constant: 234, 0xff
     -- Boolean        { }, --   A boolean constant: TRUE, false
     -- Float          { }, --   A floating point constant: 2.3e10
 
-    Identifier { fg = nord_frost_4 },      -- (*) Any variable name
-    Function { fg = nord_aurora_green },   --   Function name (also: methods for classes)
+    Identifier { Normal }, -- (*) Any variable name
+    Function { Normal },   --   Function name (also: methods for classes)
 
-    Statement { fg = nord_aurora_red }, -- (*) Any statement
+    Statement { Normal },  -- (*) Any statement
     -- Conditional    { }, --   if, then, else, endif, switch, etc.
     -- Repeat         { }, --   for, do, while, etc.
     -- Label          { }, --   case, default, etc.
@@ -199,8 +199,8 @@ local theme = lush(function(injected_functions)
 
     -- Underlined     { gui = "underline" }, -- Text that stands out, HTML links
     -- Ignore         { }, -- Left blank, hidden |hl-Ignore| (NOTE: May be invisible here in template)
-    Error { fg = nord_aurora_red },             -- Any erroneous construct
-    Todo { fg = nord_aurora_yellow },           -- Anything that needs extra attention; mostly the keywords TODO FIXME and XXX
+    Error { fg = nord_aurora_red },   -- Any erroneous construct
+    Todo { fg = nord_aurora_yellow }, -- Anything that needs extra attention; mostly the keywords TODO FIXME and XXX
 
     -- These groups are for the native LSP client and diagnostic system. Some
     -- other LSP clients may use these groups, or use their own. Consult your
