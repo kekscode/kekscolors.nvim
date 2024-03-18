8c1d1008ce6b0f44d955198163aef228a2149a9d
Stefan Antoni <stefan.antoni@deutschebahn.com>
2024-03-18 19:21:25 +0100

    chore: add+commit+pull+push all changes

--- lua/lush_theme/kekscolors.lua
+++ lua/lush_theme/kekscolors.lua
@@ -84,10 +84,10 @@ local theme = lush(function(injected_functions)
     -- Cursor         { }, -- Character under the cursor
     Visual { fg = nord_polar_night_1, bg = nord_aurora_green },  -- Visual mode selection
     -- VisualNOS { Visual },                                       -- Visual mode selection when vim is "Not Owning the Selection".
-    Search { fg = nord_polar_night_1, bg = nord_aurora_yellow }, -- Last search pattern highlighting (see 'hlsearch'). Also used for similar items that need to stand out.
+    Search { fg = nord_polar_night_1, bg = nord_aurora_purple }, -- Last search pattern highlighting (see 'hlsearch'). Also used for similar items that need to stand out.
     IncSearch { fg = nord_aurora_orange, reverse = true },       -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
-    CurSearch { fg = nord_aurora_red, reverse = true },          -- Highlighting a search pattern under the cursor (see 'hlsearch')
-    Substitute { fg = nord_aurora_red, reverse = true },         -- |:substitute| replacement text highlighting
+    CurSearch { fg = nord_aurora_orange, reverse = true },          -- Highlighting a search pattern under the cursor (see 'hlsearch')
+    Substitute { fg = nord_aurora_orange, reverse = true },         -- |:substitute| replacement text highlighting
     ScrollView { fg = nord_polar_night_2, reverse = true },      -- scrollbar
     -- lCursor        { }, -- Character under the cursor when |language-mapping| is used (see 'guicursor')
     -- CursorIM       { }, -- Like Cursor, but used when in IME mode |CursorIM|
