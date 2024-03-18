c3e744f805d4014dfbc1a13271077fbe5504f1e6
Stefan Antoni <stefan.antoni@deutschebahn.com>
2024-03-18 15:06:56 +0100

    chore: add+commit+pull+push all changes

--- lua/lush_theme/kekscolors.lua
+++ lua/lush_theme/kekscolors.lua
@@ -79,12 +79,12 @@ local theme = lush(function(injected_functions)
     --
     -- See :h highlight-groups
     --
-    Visual { fg = nord_polar_night_1, bg = nord_aurora_green }, -- Visual mode selection
-    VisualNOS { Visual },                                       -- Visual mode selection when vim is "Not Owning the Selection".
     -- ColorColumn    { }, -- Columns set with 'colorcolumn'
     -- Conceal        { }, -- Placeholder characters substituted for concealed text (see 'conceallevel')
     -- Cursor         { }, -- Character under the cursor
-    CurSearch { fg = nord_aurora_orange, reverse = true }, -- Highlighting a search pattern under the cursor (see 'hlsearch')
+    Visual { fg = nord_polar_night_1, bg = nord_aurora_green }, -- Visual mode selection
+    VisualNOS { Visual },                                       -- Visual mode selection when vim is "Not Owning the Selection".
+    CurSearch { fg = nord_aurora_orange, reverse = true },      -- Highlighting a search pattern under the cursor (see 'hlsearch')
     -- lCursor        { }, -- Character under the cursor when |language-mapping| is used (see 'guicursor')
     -- CursorIM       { }, -- Like Cursor, but used when in IME mode |CursorIM|
     -- CursorColumn   { }, -- Screen-column at the cursor, when 'cursorcolumn' is set.
@@ -102,12 +102,12 @@ local theme = lush(function(injected_functions)
     -- Folded         { }, -- Line used for closed folds
     -- FoldColumn     { }, -- 'foldcolumn'
     -- SignColumn     { }, -- Column where |signs| are displayed
-    IncSearch { fg = nord_aurora_yellow },      -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
-    Substitute { bg = nord_aurora_yellow },     -- |:substitute| replacement text highlighting
-    LineNr { fg = nord_polar_night_4 },         -- Line number for ":number" and ":#" commands, and when 'number' or 'relativenumber' option is set.
-    LineNrAbove { fg = nord_polar_night_4 },    -- Line number for when the 'relativenumber' option is set, above the cursor line
-    LineNrBelow { fg = nord_polar_night_4 },    -- Line number for when the 'relativenumber' option is set, below the cursor line
-    CursorLineNr { fg = nord_polar_night_4 },   -- Like LineNr when 'cursorline' or 'relativenumber' is set for the cursor line.
+    IncSearch { fg = nord_aurora_yellow },    -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
+    Substitute { bg = nord_aurora_yellow },   -- |:substitute| replacement text highlighting
+    LineNr { fg = nord_polar_night_4 },       -- Line number for ":number" and ":#" commands, and when 'number' or 'relativenumber' option is set.
+    LineNrAbove { fg = nord_polar_night_4 },  -- Line number for when the 'relativenumber' option is set, above the cursor line
+    LineNrBelow { fg = nord_polar_night_4 },  -- Line number for when the 'relativenumber' option is set, below the cursor line
+    CursorLineNr { fg = nord_polar_night_4 }, -- Like LineNr when 'cursorline' or 'relativenumber' is set for the cursor line.
     -- CursorLineFold { }, -- Like FoldColumn when 'cursorline' is set for the cursor line
     -- CursorLineSign { }, -- Like SignColumn when 'cursorline' is set for the cursor line
     MatchParen { reverse = true }, -- Character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
@@ -131,7 +131,7 @@ local theme = lush(function(injected_functions)
     -- PmenuThumb     { }, -- Popup menu: Thumb of the scrollbar.
     -- Question       { }, -- |hit-enter| prompt and yes/no questions
     QuickFixLine { Visual },       -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
-    Search { nord_aurora_orange }, -- Last search pattern highlighting (see 'hlsearch'). Also used for similar items that need to stand out.
+    Search { bg = nord_aurora_orange }, -- Last search pattern highlighting (see 'hlsearch'). Also used for similar items that need to stand out.
     -- SpecialKey     { }, -- Unprintable characters: text displayed differently from what it really is. But not 'listchars' whitespace. |hl-Whitespace|
     -- SpellBad       { }, -- Word that is not recognized by the spellchecker. |spell| Combined with the highlighting used otherwise.
     -- SpellCap       { }, -- Word that should start with a capital. |spell| Combined with the highlighting used otherwise.
